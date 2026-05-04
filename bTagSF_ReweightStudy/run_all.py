e9662f248c8bb4df384bd5d9bd2b2ad1e8350bf5
jhlee <jhlee@jhleeui-MacBookPro.local>
2026-05-04 22:20:07 +0900

    new SF

--- bTagSF_ReweightStudy/run_all.py
+++ bTagSF_ReweightStudy/run_all.py
@@ -32,17 +32,18 @@ from typing import List, Dict, Tuple
 # ============================================================================
 SAMPLES: List[str] = [
     # Data: BTagCSV
-    "BTagCSV_B",  "BTagCSV_C",  "BTagCSV_D",  "BTagCSV_E",  "BTagCSV_F",
+##    "BTagCSV_B",  "BTagCSV_C",  "BTagCSV_D",  "BTagCSV_E",  "BTagCSV_F",
     # Data: JetHT
-    "JetHT_B",    "JetHT_C",    "JetHT_D",    "JetHT_E",    "JetHT_F",
+##    "JetHT_B",    "JetHT_C",    "JetHT_D",    "JetHT_E",    "JetHT_F",
     # MC: ttbar inclusive
-    "TTToHadronic",  "TTTo2L2Nu",  "TTToSemiLeptonic",
+##    "TTToHadronic",  "TTTo2L2Nu",  "TTToSemiLeptonic",
     # MC: signal & rare
-    "ttHH",  "tt4b",  "ttHtobb",  "tttt",  "tttW",
-    "ttWH",  "ttWW",  "ttWZ",  "ttZHto4b",  "ttZtobb",  "ttZZto4b",  "ttbb",
+##    "ttHH",  "tt4b",  "ttHtobb",  "tttt",  "tttW",
+##    "ttWH",  "ttWW",  "ttWZ",  "ttZHto4b",  "ttZtobb",  "ttZZto4b",  "ttbb",
     # MC: QCD
-    "QCD_HT200to300",  "QCD_HT300to500",  "QCD_HT500to700",
-    "QCD_HT700to1000",  "QCD_HT1000to1500",  "QCD_HT1500to2000",  "QCD_HT2000toInf",
+##    "QCD_HT200to300",  "QCD_HT300to500",  "QCD_HT500to700",
+##    "QCD_HT700to1000",  "QCD_HT1000to1500",  "QCD_HT1500to2000",  "QCD_HT2000toInf",
+    "ttZZto4b"
 ]
 
 # ============================================================================
